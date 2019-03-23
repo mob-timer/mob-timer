e08287df51f22f19656ad18080c9ca5a94331e49
Daniel Ignat <dignite@users.noreply.github.com>
2019-03-23 21:47:30 +0100

    Ternary expressions to reduce noise

--- src/windows/window-snapper.js
+++ src/windows/window-snapper.js
@@ -19,48 +19,34 @@ const setupThresholdCheck = snapThreshold => (a, b) => {
   return Math.abs(a - b) <= snapThreshold
 }
 
-const snapLeftCheck = (windowBounds, screenBounds, isWithinThreshold) => {
-  if (isWithinThreshold(windowBounds.x, screenBounds.x)) {
-    return {
-      x: screenBounds.x,
-      shouldSnap: true
-    }
-  }
-  return {}
-}
+const snapLeftCheck = (windowBounds, screenBounds, isWithinThreshold) =>
+  isWithinThreshold(windowBounds.x, screenBounds.x) ? {
+    x: screenBounds.x,
+    shouldSnap: true
+  } : undefined
 
 const snapRightCheck = (windowBounds, screenBounds, isWithinThreshold) => {
   const rightWindowEdge = windowBounds.x + windowBounds.width
   const rightScreenEdge = screenBounds.x + screenBounds.width
-  if (isWithinThreshold(rightWindowEdge, rightScreenEdge)) {
-    return {
-      x: rightScreenEdge - windowBounds.width,
-      shouldSnap: true
-    }
-  }
-  return {}
+  return isWithinThreshold(rightWindowEdge, rightScreenEdge) ? {
+    x: rightScreenEdge - windowBounds.width,
+    shouldSnap: true
+  } : undefined
 }
 
-const snapTopCheck = (windowBounds, screenBounds, isWithinThreshold) => {
-  if (isWithinThreshold(windowBounds.y, screenBounds.y)) {
-    return {
-      y: screenBounds.y,
-      shouldSnap: true
-    }
-  }
-  return {}
-}
+const snapTopCheck = (windowBounds, screenBounds, isWithinThreshold) =>
+  isWithinThreshold(windowBounds.y, screenBounds.y) ? {
+    y: screenBounds.y,
+    shouldSnap: true
+  } : undefined
 
 const snapBottomCheck = (windowBounds, screenBounds, isWithinThreshold) => {
   const bottomWindowEdge = windowBounds.y + windowBounds.height
   const bottomScreenEdge = screenBounds.y + screenBounds.height
-  if (isWithinThreshold(bottomWindowEdge, bottomScreenEdge)) {
-    return {
-      y: bottomScreenEdge - windowBounds.height,
-      shouldSnap: true
-    }
-  }
-  return {}
+  return isWithinThreshold(bottomWindowEdge, bottomScreenEdge) ? {
+    y: bottomScreenEdge - windowBounds.height,
+    shouldSnap: true
+  } : undefined
 }
 
 module.exports = snapCheck

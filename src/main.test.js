a9ce0d5238bdb41c99660e5c87c39d25b4e0ad31
Daniel Ignat <dignite@users.noreply.github.com>
2019-09-07 17:41:27 +0200

    Test registration of ipcMain message listeners

---
+++ src/main.test.js
@@ -0,0 +1,31 @@
+const { ipcMain } = require("electron");
+
+describe("main", () => {
+  it("should register main process listeners", () => {
+    require("./main");
+    expect(ipcMain.on.mock.calls).toEqual(
+      expect.arrayContaining([
+        ["setShuffleMobbersOnStartup", expect.any(Function)],
+        ["timerWindowReady", expect.any(Function)],
+        ["configWindowReady", expect.any(Function)],
+        ["fullscreenWindowReady", expect.any(Function)],
+        ["pause", expect.any(Function)],
+        ["unpause", expect.any(Function)],
+        ["skip", expect.any(Function)],
+        ["startTurn", expect.any(Function)],
+        ["configure", expect.any(Function)],
+        ["shuffleMobbers", expect.any(Function)],
+        ["addMobber", expect.any(Function)],
+        ["removeMobber", expect.any(Function)],
+        ["updateMobber", expect.any(Function)],
+        ["setSecondsPerTurn", expect.any(Function)],
+        ["setSecondsUntilFullscreen", expect.any(Function)],
+        ["setSnapThreshold", expect.any(Function)],
+        ["setAlertSoundTimes", expect.any(Function)],
+        ["setAlertSound", expect.any(Function)],
+        ["setTimerAlwaysOnTop", expect.any(Function)],
+        ["setShuffleMobbersOnStartup", expect.any(Function)]
+      ])
+    );
+  });
+});
